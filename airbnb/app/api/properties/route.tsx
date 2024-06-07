ed331f8738dc01cb0df7c6e7409385c6aed8800d
Mohammad Azarbayejani <azarmoh81@gmail.com>
2024-06-07 21:00:59 +0330

    location added to api of properties

--- airbnb/app/api/properties/route.tsx
+++ airbnb/app/api/properties/route.tsx
@@ -3,6 +3,14 @@ import prisma from "@/prisma/client";
 
 // this get method for getting properties
 export async function GET(request: NextRequest) {
-  const properties = await prisma.properties.findMany();
+  const properties = await prisma.properties.findMany(
+    // join this table to locations table 
+    {
+      include: {
+        Location: true,
+      },
+    }
+  );
+
   return NextResponse.json(properties);
 }
